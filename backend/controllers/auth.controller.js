082eac6ada96f8bfe72d748b7863c291b8b28f51
unknown <mu0826960@gmail.com>
2026-02-10 22:33:04 +0500

    E-commerce website setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req,res) =>{
+    res.send("Sign up request");
+}
+
+export const login = async (req,res) =>{
+    res.send("Login request");
+}
+
+export const logout = async (req,res) =>{
+    res.send("Logout request");
+}
